c20d97167bf5dccc6385c593cb3fbcef01744dca
kibet kilel <amos.rotich@strathmore.edu>
2021-08-02 08:07:46 +0300

    database and admin panel

--- venus/myapp/models.py
+++ venus/myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Feature:
-    id: int
-    name: str
-    details: str
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=100)
+
--- venus/myapp/views.py
+++ venus/myapp/views.py
@@ -5,34 +5,35 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.details = 'Our services are quick'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Fast'
-    feature2.details = 'Our services are quick'
-
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Reliable'
-    feature3.details = 'Our services are Reliable'
-
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Easy to use'
-    feature4.details = 'Our services are Easy to use'
-
-
-    feature5 = Feature()
-    feature5.id = 4
-    feature5.name = 'Affordable'
-    feature5.details = 'Our services are Affordable'
-    features = [feature1, feature2, feature3, feature4]
+    features = Feature.object.all()
+    # feature1 = Feature()
+    # feature1.id = 0
+    # feature1.name = 'Fast'
+    # feature1.details = 'Our services are quick'
+    #
+    # feature2 = Feature()
+    # feature2.id = 1
+    # feature2.name = 'Fast'
+    # feature2.details = 'Our services are quick'
+    #
+    #
+    # feature3 = Feature()
+    # feature3.id = 2
+    # feature3.name = 'Reliable'
+    # feature3.details = 'Our services are Reliable'
+    #
+    #
+    # feature4 = Feature()
+    # feature4.id = 3
+    # feature4.name = 'Easy to use'
+    # feature4.details = 'Our services are Easy to use'
+    #
+    #
+    # feature5 = Feature()
+    # feature5.id = 4
+    # feature5.name = 'Affordable'
+    # feature5.details = 'Our services are Affordable'
+    # features = [feature1, feature2, feature3, feature4]
 
     # feature1 = Feature()
     # feature1.id = 5
