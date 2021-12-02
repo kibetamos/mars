6351f90ed9a6ca8ad1c3dff04483b00ed7f67540
kibet kilel <amos.rotich@strathmore.edu>
2021-12-02 18:35:25 +0300

    add login

--- venus/myapp/urls.py
+++ venus/myapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index1'),
     path('index', views.index, name='index'),
-    path('index1', views.index, name='index1'),
     path('counter', views.counter, name='counter')
 
 ]
