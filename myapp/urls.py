46d94d21e1db83725f504811c5631f6dc60f0cfe
kibetamos <amos.rotich@strathmore.edu>
2023-10-24 12:07:54 +0300

    Update: added gitignore

---
+++ myapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('register', views.register, name="register"),
+    path('login', views.login, name="login"),
+    path('home', views.home, name='home'),
+    path('counter', views.counter, name='counter'),
+
+
+]
